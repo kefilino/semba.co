74dc0909461f861c0f19a25e35d5ea0fad02ae70
sheilaazhar <47959466+sheilaazhar@users.noreply.github.com>
2020-11-16 22:03:54 +0700

    add table bantuan

---
+++ database/migrations/2020_11_16_135223_create_bantuan_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBantuanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bantuan', function (Blueprint $table) {
+            $table->string('id_bantuan')->unique();
+            $table->string('nama_bantuan');
+            $table->string('persediaan');
+            $table->string('foto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bantuan');
+    }
+}
