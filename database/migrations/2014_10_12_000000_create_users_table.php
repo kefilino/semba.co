04d76458b1a14d445b819d2b7716861f68535d72
Kefilino Khalifa <kefilino@live.com>
2020-11-16 21:11:26 +0700

    alter create-users-table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nik')->unique();
+            $table->string('kk');
+            $table->string('nama');
+            $table->string('kode_pos');
+            $table->string('alamat');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
