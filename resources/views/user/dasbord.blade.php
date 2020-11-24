11b49327e987f2215e11e7a0ee25a37fcedc636e
sarahnvnt <47983951+sarahnvnt@users.noreply.github.com>
2020-11-25 00:00:35 +0700

    dashboard user

---
+++ resources/views/user/dasbord.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<title>Semba.co</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="dasborduser.css">
+
+
+<body >
+    <!-- Top container -->
+    <div class="w3-bar w3-top w3-white w3-large" style="z-index:4">
+        <button class="w3-bar-item w3-button w3-hide-large w3-hover-none w3-hover-text-light-grey" onclick="w3_open();"><i class="fa fa-bars"></i>  Menu</button>
+        <a href="#" span class="w3-bar-item w3-left">Semba.co</span>
+    </div>
+
+   <!-- Sidebar/menu -->
+<nav class="w3-sidebar w3-collapse w3-blue w3-animate-left" style="z-index:3;width:250px;" id="mySidebar"><br>
+    <div class="w3-container">
+    <div class="w3-bar-block">
+      <a href="#" class="w3-bar-item w3-button w3-padding-16 w3-hide-large w3-dark-grey w3-hover-black" onclick="w3_close()" title="close menu"><i class="fa fa-remove fa-fw"></i>  Close Menu</a>
+      <a href="#" class="w3-bar-item w3-button w3-padding w3-blue"><i class="fa fa-home "></i>&ensp;&ensp;Dashboard</a>
+      <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-cogs"></i>&ensp; Pengaturan</a>
+      <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-ticket"></i>&ensp; Permintaan</a>
+      <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-pencil-square-o"></i>&ensp; Edit Profil</a>
+      <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-sign-out"></i>&ensp; Keluar</a>
+    </div>
+  </nav>
+
+
+    <!-- Overlay effect when opening sidebar on small screens -->
+    <div class="w3-overlay w3-hide-large w3-animate-opacity" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+
+    <!-- !PAGE CONTENT! -->
+    <div class="w3-main" style="margin-left:300px;margin-top:43px;">
+
+        <!-- Header -->
+        <header class="w3-container" style="padding-top:22px">
+            <h5><b><i class="#"></i></b></h5>
+        </header>
+
+        <div class="w-1-padding  ">
+            <div class="w3-quarter">
+                    <div id="card1">
+                        <div class="w3-threequarter">
+                            <div id="card1-content">
+                                <h3>Permintaan Anda</h3>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="w3-clear"></div>
+                    <h4></h4>
+                </div>
+            </div>
+        
+        
+        <header class="w3-container" style="padding-top:22px">
+            <h5><b><i class="#"></i></b></h5>
+        </header>
+
+        <div class="w-1-padding  ">
+            <div class="w3-quarter">
+                <div class="w3-center"><img src=""></i></div>
+                    <div id="card1">
+                        <div class="w3-threequarter">
+                            <div id="card1-content">
+                                <h3> Berita Semba.co</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="w3-clear"></div>
+                    <h4></h4>
+                </div>
+            </div>
+        </div>
+        <!-- End page content -->
+
+
+</body>
+
+</html>
